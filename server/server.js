74e88872003cb3dbf2e0ff4eb5a9c48942929da8
romulomelendez <romulomelendez.97@gmail.com>
2021-10-15 08:48:22 -0300

    creating and using a html page

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 app.get('/', ( req, res ) => {
 
-    res.send('Olá mundo!')
-    
+    res.sendFile(path.join(__dirname, '../src/index.html'))
+
 })
+
+app.listen(8081)
