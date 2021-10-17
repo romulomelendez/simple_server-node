2f56b05e90156551e96839ccf65317972158b743
romulomelendez <romulomelendez.97@gmail.com>
2021-10-17 00:35:18 -0300

    organizing folders and files

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+//  Static Files
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/', ( req, res ) => {
 
     res.sendFile(path.join(__dirname, '../src/index.html'))
