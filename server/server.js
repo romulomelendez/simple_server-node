ee815dad2c221ef05e5a8cdf887b86462ac5ca0d
romulomelendez <romulomelendez.97@gmail.com>
2021-12-05 19:34:51 -0300

    update server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express()
 const path = require('path')
 
 //  Static Files
-app.use(express.static(path.join(__dirname, 'public')))
+//app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', ( req, res ) => {
 
